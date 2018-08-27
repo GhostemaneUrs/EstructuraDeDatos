e2e02aad883f2efe05db8b4258727a67f5a2f396
GhostemaneUrs <JaramilloG1999@outlook.com>
2018-08-26 22:42:56 -0500

    terminando el proyecto leer y escribir archivos json en java

--- Write And Read JSON/src/edu/cuc/json/CreateJSONFile.java
+++ Write And Read JSON/src/edu/cuc/json/CreateJSONFile.java
@@ -1,14 +1,41 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Esta clase se va usar para aprender el metodo de crear archivos JSON en java
  */
 package edu.cuc.json;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 /**
  *
- * @author Jaram
+ * @author Ghostemane
  */
 public class CreateJSONFile {
-    
+    public static void main (String [] args){
+        JSONObject objeto = new JSONObject();        //Se puede crear por objeto, via clave y ingreso
+        
+        objeto.put("Nombre",  "Andrés Felipe");
+        objeto.put("Apellidos",  "Jaramillo Gonzalez");
+        objeto.put("Nacionalidad",  "Colombia");
+        objeto.put("Edad",  "19 años");
+        
+        
+        JSONArray lista = new JSONArray();   //Se puede crear por lista
+        
+        lista.add("Tu mama es mia");
+        lista.add("El que madruga Dios lo ayuda, pero el que no duerme se come el desayuno del que madruga");
+        lista.add("El mejor mix que tengo https://www.youtube.com/watch?v=4iJJCaSyoSw&start_radio=1&list=RDMM4iJJCaSyoSw");
+        
+        objeto.put("Frase del dia", lista);
+        
+        try (FileWriter archivo = new FileWriter("Archivo.json"))  //Usamos un try, catch para evitar error al guardar el archivo
+        {
+              archivo.write(objeto.toString()); // El archivo que creamos le pasamo los objetos que realizamos convirtiendolos en un toString 
+              archivo.flush();                             // para poder visualizarlo
+        } catch (IOException e ) {
+        }
+        System.out.println(objeto);
+    }
 }
--- Write And Read JSON/src/edu/cuc/json/ReadJSONFile.java
+++ Write And Read JSON/src/edu/cuc/json/ReadJSONFile.java
@@ -1,14 +1,58 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Esta clase se usara para aprender a leer archivos JSON en java
  */
 package edu.cuc.json;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
 /**
  *
- * @author Jaram
+ * @author Ghostemane
  */
 public class ReadJSONFile {
-    
+    public static void main(String[] args) {
+         
+        JSONParser parser = new JSONParser();
+        
+        try {
+             
+            // Para leer archivos JSON en objetos
+            
+             Object objeto = parser.parse(new FileReader("Archivo.json"));
+             JSONObject jsonObjeto = (JSONObject) objeto;
+             String nombre = (String) jsonObjeto.get("Nombre");
+             System.out.println("Nombre: " + nombre);
+             
+             String apellido = (String) jsonObjeto.get("Apellidos");
+             System.out.println("Apellido: " + apellido);
+             
+             String nacionalidad = (String) jsonObjeto.get("Nacionalidad");
+             System.out.println("Nacionalidad: " + nacionalidad);
+             
+             String edad = (String) jsonObjeto.get("Edad");
+             System.out.println("Edad: " + edad);
+             
+             //Para leer archivos JSON en un Array 
+            
+             JSONArray listaPrueba  = (JSONArray) jsonObjeto.get("Frase del dia");
+             Iterator<String> iterator = listaPrueba.iterator();
+             while (iterator.hasNext()) {
+                 System.out.println("Frases del dia: " + iterator.next()); 
+            }
+             
+             
+        } catch (FileNotFoundException e) {}
+         catch (IOException e) {}
+         catch (ParseException e) {}
+         catch (Exception e) {}
+    }
 }
